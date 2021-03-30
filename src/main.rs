b8f419661733ee871188934df54035429c5f894c
Jiajie Chen <c@jia.je>
2021-03-30 16:13:32 +0800

    Update prefixes and fix optional strings

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ struct Prefix {
     ip: String,
     cidr: u32,
     roa_status: String,
-    name: String,
-    description: String,
+    name: Option<String>,
+    description: Option<String>,
     country_code: String,
 }
 
@@ -32,28 +32,32 @@ struct Data {
 
 fn main() -> io::Result<()> {
     let mut vec: Vec<IPAddress> = Vec::new();
-    for file in ["as4538_prefixes", "as23910_prefixes", "as24348_prefixes", "as45576_prefixes"].iter() {
+    for file in [
+        "as4538_prefixes",
+        "as23910_prefixes",
+        "as24348_prefixes",
+        "as45576_prefixes",
+    ]
+    .iter()
+    {
         let mut f = File::open(file)?;
         let mut buffer = String::new();
         f.read_to_string(&mut buffer)?;
         let data: Data = serde_json::from_str(&buffer).unwrap();
         for prefix in data.data.ipv4_prefixes.iter() {
-            if prefix.name.contains("TSINGHUA")
-                || prefix.name.contains("TUZJ")
-                || prefix.name.contains("TUNET")
-            {
-                vec.push(IPAddress::parse(prefix.prefix.clone()).unwrap());
+            if let Some(name) = &prefix.name {
+                if name.contains("TSINGHUA") || name.contains("TUZJ") || name.contains("TUNET") {
+                    vec.push(IPAddress::parse(prefix.prefix.clone()).unwrap());
+                }
             }
         }
         for prefix in data.data.ipv6_prefixes.iter() {
-            if prefix.name.contains("TSINGHUA")
-                || prefix.name.contains("TUZJ")
-                || prefix.name.contains("TUNET")
-            {
-                vec.push(IPAddress::parse(prefix.prefix.clone()).unwrap());
+            if let Some(name) = &prefix.name {
+                if name.contains("TSINGHUA") || name.contains("TUZJ") || name.contains("TUNET") {
+                    vec.push(IPAddress::parse(prefix.prefix.clone()).unwrap());
+                }
             }
         }
-
     }
     let aggregated = IPAddress::aggregate(&vec);
 
